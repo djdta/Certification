a22110cda25191560c743620c242e0d41fdd12af
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-01-02 16:44:37 +0000

    update

--- Hashicorp/Terraform Associate/3 Understand Terraform basics/versioning.tf
+++ Hashicorp/Terraform Associate/3 Understand Terraform basics/versioning.tf
@@ -1,11 +1,34 @@
 # Example of a provider using a version for this provider.
 
 # Azure
-provider "azurerm" {
-  version = "=2.40.0"
-  features {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
 }
 
 # AWS Provider
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0.4"
+    }
+  }
+}
+
 # GCP Provider
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.51.0"
+    }
+  }
+}
