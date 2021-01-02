c2fa302ccc60f2168b1a889684c6d8ec24d74289
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-01-02 17:03:37 +0000

    update

---
+++ Hashicorp/Terraform Associate/4 Use the Terraform CLI (outside of core workflow)/fmt.tf
@@ -0,0 +1,32 @@
+# Example of what the fmt command can do to the terraform code
+
+## Example 1
+
+# Before
+provider "aws" {
+  version = "~> 3.0"
+  region = "us-east-1"
+}
+
+# After
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-east-1"
+}
+
+# Example 2
+
+# Before
+provider "aws" {
+  region = "us-west-2"
+  shared_credentials_file = "/Users/tf_user/.aws/creds"
+  profile = "customprofile"
+}
+
+# After
+provider "aws" {
+  region                  = "us-west-2"
+  shared_credentials_file = "/Users/tf_user/.aws/creds"
+  profile                 = "customprofile"
+}
+
