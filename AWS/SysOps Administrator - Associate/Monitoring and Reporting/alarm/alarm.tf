f20e495464bc40ed093d550dda867b8fe1b52427
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-03-26 18:07:52 +0000

    update alarm.tf

--- AWS/SysOps Administrator - Associate/Monitoring and Reporting/alarm/alarm.tf
+++ AWS/SysOps Administrator - Associate/Monitoring and Reporting/alarm/alarm.tf
@@ -1,12 +1,21 @@
 resource "aws_cloudwatch_composite_alarm" "example" {
   alarm_description = "This is a composite alarm!"
-  alarm_name        = "example-composite-alarm"
-
-  alarm_actions = aws_sns_topic.example.arn
-  ok_actions    = aws_sns_topic.example.arn
+  alarm_name        = "ec2-composite-alarm"
 
   alarm_rule = <<EOF
-ALARM(${aws_cloudwatch_metric_alarm.alpha.alarm_name}) OR
-ALARM(${aws_cloudwatch_metric_alarm.bravo.alarm_name})
+(ALARM(${aws_cloudwatch_metric_alarm.alpha.alarm_name}))
 EOF
+}
+
+resource "aws_cloudwatch_metric_alarm" "alpha" {
+  alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
 }
