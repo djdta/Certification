895cbec6827f393a1b967a41f15fd31aa0f3b94b
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-03-29 18:18:11 +0100

    added lb files

--- AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.tf
+++ AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.tf
@@ -158,3 +158,34 @@ resource "aws_lb_listener" "example" {
     type             = "forward"
   }
 }
+
+# Instance Target Group
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# IP Target Group
+resource "aws_lb_target_group" "ip-example" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Lambda Target Group
+resource "aws_lb_target_group" "lambda-example" {
+  name        = "tf-example-lb-tg"
+  target_type = "lambda"
+}
