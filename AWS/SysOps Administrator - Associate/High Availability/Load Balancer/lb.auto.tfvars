78f178377178dd1983cc7fb5ad2469010bd11cc3
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-03-30 17:51:09 +0100

    Udated Load Balancer tf files

--- AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.auto.tfvars
+++ AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.auto.tfvars
@@ -1,3 +1,3 @@
 Application_Load_Balancer = false
 Network_Load_Balancer = false
-Classic_load_balancer = false
+Classic_Load_balancer = false
--- AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.tf
+++ AWS/SysOps Administrator - Associate/High Availability/Load Balancer/lb.tf
@@ -1,148 +1,53 @@
+data "aws_vpc" "id" {
+  id = var.vpc_id
+}
+
+data "aws_subnet" "example1" {
+  id = var.subnet1
+}
+
+data "aws_subnet" "example2" {
+  id = var.subnet2
+}
+
+data "aws_subnet" "example3" {
+  id = var.subnet3
+}
+
 resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+  count              = var.Application_Load_Balancer == true ? 1 : 0
+  name               = "ALB-01"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  # security_groups    = [data.aws_security_group.lb.id]
+  subnets = [data.aws_subnet.example1.id, data.aws_subnet.example2.id, data.aws_subnet.example3.id]
 
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
+  enable_deletion_protection = false
 
   tags = {
-    Environment = "production"
+    Environment = "ALB-01"
   }
 }
 
-# Network Load Balancer
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = aws_subnet.public.*.id
+resource "aws_lb_listener" "example" {
+  count             = var.Application_Load_Balancer == true ? 1 : 0
+  load_balancer_arn = aws_lb.test[0].id
+  port              = "80"
+  protocol          = "HTTP"
 
-  enable_deletion_protection = true
-
-  tags = {
-    Environment = "production"
-  }
-}
-
-## Specifying Elastic IPs
-resource "aws_lb" "example" {
-  name               = "example"
-  load_balancer_type = "network"
-
-  subnet_mapping {
-    subnet_id     = aws_subnet.example1.id
-    allocation_id = aws_eip.example1.id
-  }
-
-  subnet_mapping {
-    subnet_id     = aws_subnet.example2.id
-    allocation_id = aws_eip.example2.id
-  }
-}
-
-# Specifying private IP addresses for an internal-facing load balancer
-resource "aws_lb" "example" {
-  name               = "example"
-  load_balancer_type = "network"
-
-  subnet_mapping {
-    subnet_id            = aws_subnet.example1.id
-    private_ipv4_address = "10.0.1.15"
-  }
-
-  subnet_mapping {
-    subnet_id            = aws_subnet.example2.id
-    private_ipv4_address = "10.0.2.15"
-  }
-}
-
-## Forward Action
-resource "aws_lb" "front_end" {
-  # ...
-}
-
-resource "aws_lb_target_group" "front_end" {
-  # ...
-}
-
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
+    target_group_arn = aws_lb_target_group.example[0].id
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
-  }
-}
-
-## Redirect Action
-resource "aws_lb" "front_end" {
-  # ...
-}
-
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-# Fixed-response Action
-resource "aws_lb" "front_end" {
-  # ...
-}
-
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
-
-# Gateway Load Balancer Listener
-resource "aws_lb" "example" {
-  load_balancer_type = "gateway"
-  name               = "example"
-
-  subnet_mapping {
-    subnet_id = aws_subnet.example.id
   }
 }
 
 resource "aws_lb_target_group" "example" {
-  name     = "example"
-  port     = 6081
-  protocol = "GENEVE"
-  vpc_id   = aws_vpc.example.id
+  count    = var.Application_Load_Balancer == true ? 1 : 0
+  name     = "Web-Servers"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.id.id
 
   health_check {
     port     = 80
@@ -150,80 +55,43 @@ resource "aws_lb_target_group" "example" {
   }
 }
 
-resource "aws_lb_listener" "example" {
-  load_balancer_arn = aws_lb.example.id
+## Network Load Balancer
+resource "aws_lb" "network_lb" {
+  count              = var.Network_Load_Balancer == true ? 1 : 0
+  name               = "NLB-01"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [data.aws_subnet.example1.id, data.aws_subnet.example2.id, data.aws_subnet.example3.id]
 
-  default_action {
-    target_group_arn = aws_lb_target_group.example.id
-    type             = "forward"
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "NLB-01"
   }
 }
 
-# Instance Target Group
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
+# Create a new Classic load balancer
+resource "aws_elb" "bar" {
+  count             = var.Classic_Load_balancer == true ? 1 : 0
+  name               = "CLB-01"
+  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 
-# IP Target Group
-resource "aws_lb_target_group" "ip-example" {
-  name        = "tf-example-lb-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.main.id
-}
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Lambda Target Group
-resource "aws_lb_target_group" "lambda-example" {
-  name        = "tf-example-lb-tg"
-  target_type = "lambda"
-}
-
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_instance.test.id
-  port             = 80
-}
-
-resource "aws_lb_target_group" "test" {
-  # ... other configuration ...
-}
-
-resource "aws_instance" "test" {
-  # ... other configuration ...
-}
-
-# Usage with lambda
-resource "aws_lambda_permission" "with_lb" {
-  statement_id  = "AllowExecutionFromlb"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.test.arn
-  principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn    = aws_lb_target_group.test.arn
-}
-
-resource "aws_lb_target_group" "test" {
-  name        = "test"
-  target_type = "lambda"
-}
-
-resource "aws_lambda_function" "test" {
-  # ... other configuration ...
-}
-
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_lambda_function.test.arn
-  depends_on       = [aws_lambda_permission.with_lb]
+  tags = {
+    Name = "CLB-01"
+  }
 }
