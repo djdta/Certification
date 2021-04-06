1aa8a76ce30be1fcf072043b6bbbb506aae7f879
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-04-06 16:35:30 +0100

    Update EC2

--- AWS/SysOps Administrator - Associate/High Availability/Auto Scale/EC2/ec2.tf
+++ AWS/SysOps Administrator - Associate/High Availability/Auto Scale/EC2/ec2.tf
@@ -1,9 +1,3 @@
-# Feature flags
-# Conditionally based on feature flag
-#count = local.provision_b59func == true ? 1 : 0
-# If 'true', provision 1 of this resource
-# If 'false', don't provision this
-
 # With Latest Version Of Launch Template
 resource "aws_launch_template" "foobar" {
   name_prefix   = "foobar"
@@ -84,54 +78,7 @@ resource "aws_autoscaling_group" "example_refresh" {
     triggers = ["tag"]
   }
 }
-#
-#resource "aws_autoscaling_group" "bar" {
-#  name                      = "foobar3-terraform-test"
-#  max_size                  = 5
-#  min_size                  = 2
-#  health_check_grace_period = 300
-#  health_check_type         = "ELB"
-#  desired_capacity          = 4
-#  force_delete              = true
-#  placement_group           = aws_placement_group.test.id
-#  launch_configuration      = aws_launch_configuration.foobar.name
-#  vpc_zone_identifier       = [aws_subnet.example1.id, aws_subnet.example2.id]
-#
-#  initial_lifecycle_hook {
-#    name                 = "foobar"
-#    default_result       = "CONTINUE"
-#    heartbeat_timeout    = 2000
-#    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-#
-#    notification_metadata = <<EOF
-#{
-#  "foo": "bar"
-#}
-#EOF
-#
-#    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
-#    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
-#  }
-#
-#  tag {
-#    key                 = "foo"
-#    value               = "bar"
-#    propagate_at_launch = true
-#  }
-#
-#  timeouts {
-#    delete = "15m"
-#  }
-#
-#  tag {
-#    key                 = "lorem"
-#    value               = "ipsum"
-#    propagate_at_launch = false
-#  }
-#}
-#
-#
-#
+
 ## Mixed Instances Policy with Spot Instances and Capacity Rebalance
 
 data "aws_ami" "example" {
@@ -184,77 +131,4 @@ resource "aws_autoscaling_group" "example_mixed" {
   }
 }
 
-#resource "aws_launch_template" "example2" {
-#  name_prefix = "example2"
-#  image_id    = data.aws_ami.example2.id
-#}
-#
-#resource "aws_autoscaling_group" "example" {
-#  availability_zones = ["us-east-1a"]
-#  desired_capacity   = 1
-#  max_size           = 1
-#  min_size           = 1
-#
-#  mixed_instances_policy {
-#    launch_template {
-#      launch_template_specification {
-#        launch_template_id = aws_launch_template.example.id
-#      }
-#
-#      override {
-#        instance_type     = "c4.large"
-#        weighted_capacity = "3"
-#      }
-#
-#      override {
-#        instance_type = "c6g.large"
-#        launch_template_specification {
-#          launch_template_id = aws_launch_template.example2.id
-#        }
-#        weighted_capacity = "2"
-#      }
-#    }
-#  }
-#}
-#
-#resource "aws_autoscaling_group" "bar" {
-#  name                 = "foobar3-terraform-test"
-#  max_size             = 5
-#  min_size             = 2
-#  launch_configuration = aws_launch_configuration.foobar.name
-#  vpc_zone_identifier  = [aws_subnet.example1.id, aws_subnet.example2.id]
-#
-#  tags = concat(
-#    [
-#      {
-#        "key"                 = "interpolation1"
-#        "value"               = "value3"
-#        "propagate_at_launch" = true
-#      },
-#      {
-#        "key"                 = "interpolation2"
-#        "value"               = "value4"
-#        "propagate_at_launch" = true
-#      },
-#    ],
-#    var.extra_tags,
-#  )
-#}
-#
 
-
-
-
-
-#resource "aws_autoscaling_group" "foobar" {
-#  availability_zones   = ["us-west-2a"]
-#  name                 = "terraform-test-foobar5"
-#  health_check_type    = "EC2"
-#  termination_policies = ["OldestInstance"]
-#
-#  tag {
-#    key                 = "Foo"
-#    value               = "foo-bar"
-#    propagate_at_launch = true
-#  }
-#}
