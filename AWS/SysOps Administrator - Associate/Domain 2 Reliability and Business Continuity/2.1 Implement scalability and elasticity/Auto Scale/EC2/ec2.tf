4bb76f74f9a9e355f58ae64f79a4a2396e816009
Michael Hanley <hanley_michael0918@yahoo.co.uk>
2021-11-14 21:33:22 +0000

    Terraform Files
    Updated terraform code

--- AWS/SysOps Administrator - Associate/Domain 2 Reliability and Business Continuity/2.1 Implement scalability and elasticity/Auto Scale/EC2/ec2.tf
+++ AWS/SysOps Administrator - Associate/Domain 2 Reliability and Business Continuity/2.1 Implement scalability and elasticity/Auto Scale/EC2/ec2.tf
@@ -6,136 +6,82 @@
 
 #TODO:auto scaling policy
 
-resource "aws_launch_template" "foobar" {
-  name_prefix   = "foobar"
-  image_id      = "ami-096cb92bb3580c759"
-  instance_type = "t2.micro"
-
-}
-
-resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["eu-west-2a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
-  count              = var.dev == true ? 3 : 0
-
-  launch_template {
-    id      = aws_launch_template.foobar.id
-    version = "$Latest"
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  count              = var.mgmt == true ? 3 : 0
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
-
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.foobar.id
-      }
-
-      override {
-        instance_type     = "c4.large"
-        weighted_capacity = "3"
-      }
-
-      override {
-        instance_type     = "c3.large"
-        weighted_capacity = "2"
-      }
-    }
-  }
-}
-
-## Automatically refresh all instances after the group is updated
-
-resource "aws_launch_template" "example_refresh" {
-  image_id      = data.aws_ami.example.id
-  instance_type = "t3.nano"
-}
-
-resource "aws_autoscaling_group" "example_refresh" {
-  count = var.Automatically_refresh_code_deployment == true ? 1 : 0
-  availability_zones = ["eu-west-2a"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.example_refresh.id
-    version = aws_launch_template.example_refresh.latest_version
-  }
-
-  tag {
-    key                 = "Key"
-    value               = "Value"
-    propagate_at_launch = true
-  }
-
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-}
-
-## Mixed Instances Policy with Spot Instances and Capacity Rebalance
-
-data "aws_ami" "example" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "AWS Training"
   }
 }
 
-resource "aws_launch_template" "example_mixed" {
-  name_prefix   = "example"
-  image_id      = data.aws_ami.example.id
-  instance_type = "t2.mirco"
-}
+resource "aws_subnet" "main" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.training_subnets[count.index] 
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-resource "aws_autoscaling_group" "example_mixed" {
-  count = var.Mixed_Instances_Policy_with_Spot_Instances == true ? 1 : 0
-  availability_zones = ["eu-west-2a"]
-  capacity_rebalance  = true
-  desired_capacity    = 3
-  max_size            = 3
-  min_size            = 1
-  #vpc_zone_identifier = [aws_subnet.example1.id, aws_subnet.example2.id]
-
-  mixed_instances_policy {
-    instances_distribution {
-      on_demand_base_capacity                  = 0
-      on_demand_percentage_above_base_capacity = 25
-      spot_allocation_strategy                 = "capacity-optimized"
-    }
-
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.example_mixed.id
-      }
-
-      override {
-        instance_type     = "t2.mirco"
-        weighted_capacity = "3"
-      }
-
-      override {
-        instance_type     = "t3.medium"
-        weighted_capacity = "2"
-      }
-    }
+  tags = {
+    Name = "AWS Training"
   }
 }
 
+resource "aws_launch_configuration" "config" {
+  name          = var.launch_configuration_name
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  	user_data = <<EOF
+      #! /bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apache2
+      sudo systemctl start apache2
+      sudo systemctl enable apache2
+      echo "Page served from AZ $(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)" > /var/www/html/index.html
+	EOF
+}
 
+resource "aws_autoscaling_group" "bar" {
+  name                      = var.autoscaling_group_name
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = true
+  vpc_zone_identifier       = [aws_subnet.main[0].id, aws_subnet.main[1].id, aws_subnet.main[2].id]
+  launch_configuration      = aws_launch_configuration.config.name
+
+  tag {
+    key                 = var.autoscaling_group_name
+    value               = "bar"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "lorem"
+    value               = "ipsum"
+    propagate_at_launch = true
+  }
+}
--- AWS/SysOps Administrator - Associate/Domain 2 Reliability and Business Continuity/2.1 Implement scalability and elasticity/Auto Scale/EC2/ha.auto.tfvars
+++ AWS/SysOps Administrator - Associate/Domain 2 Reliability and Business Continuity/2.1 Implement scalability and elasticity/Auto Scale/EC2/ha.auto.tfvars
@@ -1,2 +1,10 @@
-Mixed_Instances_Policy_with_Spot_Instances = false
-Automatically_refresh_code_deployment = false
+launch_configuration_name = "training-config"
+instance_type = "t2.micro"
+autoscaling_group_name = "auto-scaling-group"
+
+training_subnets = [
+  "10.0.1.0/24",
+  "10.0.2.0/24",
+  "10.0.3.0/24"
+]
+
